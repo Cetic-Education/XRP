90c1218547dd5371f4b263bed887434907bb6235
Cetic-Education <teamaccess@cetic.education>
2025-11-08 22:52:14 +0800

    Create oneclick_install_with_venv.bat

---
+++ oneclick_install_with_venv.bat
@@ -0,0 +1,23 @@
+echo off
+echo "Building venv in current, please ensure python can be used in command line \nVenv will be installed at current bat location, press y to continue process"
+choice /c YN /M "press Y to continue process, N to exit" /t 10 /d N
+@if %errorlevel% == 2 exit
+@if %errorlevel% == 1 goto next
+pause
+exit
+:next
+if exist %~dp0.venv (
+    echo ".venv exist, installing requirement" 
+    goto requirement
+) else (
+    echo "Building venv"
+    python -m venv %~dp0.venv)
+:requirement
+if exist %~dp0requirement.txt (
+    echo "Requirement.txt exist, installing"
+    %~dp0.venv\Scripts\pip install -r %~dp0requirement.txt)^
+else (echo "Requirement.txt not exist, exiting")
+
+:last
+Pause
+exit
