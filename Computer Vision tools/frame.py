c44603cc66025fbd6c5395c81c66d64b97de2a87
結露 <68598198+XketsuroX@users.noreply.github.com>
2025-12-05 01:00:51 +0800

    frame.py added

---
+++ Computer Vision tools/frame.py
@@ -0,0 +1,11 @@
+import cv2
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow("Robot Vision", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+cap.release()
+cv2.destroyAllWindows()
